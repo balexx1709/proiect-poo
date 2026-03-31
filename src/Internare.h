21295a38ba5e7801f6f781b5a24f72fb286f464b
Bigea Alexandru Gabriel <alexandru.bigea@student.usv.ro>
2026-03-31 18:52:12 +0300

    Add Internare class for hospital admission

---
+++ src/Internare.h
@@ -0,0 +1,16 @@
+#ifndef INTERNARE_H
+#define INTERNARE_H
+
+class Internare {
+public:
+    // Constructor for hospital admission
+    Internare();
+
+    // Method to admit a patient
+    void admitePacient();
+
+    // Method to discharge a patient
+    void externarePacient();
+};
+
+#endif // INTERNARE_H
