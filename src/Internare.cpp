1537d010fafb6c93bb8a423801de0d4aa8b52e42
Bigea Alexandru Gabriel <alexandru.bigea@student.usv.ro>
2026-03-31 18:53:17 +0300

    Add Internare implementation

---
+++ src/Internare.cpp
@@ -0,0 +1,15 @@
+// Implementation of Internare
+#include <iostream>
+
+class Internare {
+public:
+    void display() {
+        std::cout << "Internare implementation" << std::endl;
+    }
+};
+
+int main() {
+    Internare internare;
+    internare.display();
+    return 0;
+}
